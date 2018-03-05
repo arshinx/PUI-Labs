ec306093abdbce434d7ddca16527d4707e1f3af5
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-05 10:13:40 -0500

    Polar bear constructor

---
+++ week7_lab/assets/main.js
@@ -0,0 +1,9 @@
+
+// --- Animals --- //
+// Polar Bear
+function PolarBear(name, age) {
+  this.name = name;
+  this.age = age;
+  this.type = “MyAnimal”;
+  this.image = “anImageOfMyAnimal.jpg”;
+}
