dad92bab168d4f6f510efd58fd80e666a037f772
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-05 10:14:29 -0500

    Rabbit

--- week7_lab/assets/main.js
+++ week7_lab/assets/main.js
@@ -16,3 +16,11 @@ function PolarBear(name, age) {
   this.type = “MyAnimal”;
   this.image = “anImageOfMyAnimal.jpg”;
 }
+
+// Rabbit (Constructor)
+function Rabbit(name, age) {
+  this.name = name;
+  this.age = age;
+  this.type = “MyAnimal”;
+  this.image = “anImageOfMyAnimal.jpg”;
+}
