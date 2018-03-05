0c400f1493134ef678573ba486c2ac71abf8c8ae
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-05 10:14:10 -0500

    Lion

--- week7_lab/assets/main.js
+++ week7_lab/assets/main.js
@@ -1,6 +1,15 @@
 
 // --- Animals --- //
-// Polar Bear
+
+// Polar Bear (Constructor)
+function PolarBear(name, age) {
+  this.name = name;
+  this.age = age;
+  this.type = “MyAnimal”;
+  this.image = “anImageOfMyAnimal.jpg”;
+}
+
+// Lion (Constructor)
 function PolarBear(name, age) {
   this.name = name;
   this.age = age;
