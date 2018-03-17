b74fbe84298da599d3636c6c12193a24d9c6c5b0
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-16 22:53:50 -0400

    Pillow Model

---
+++ homework_5/js/models.js
@@ -0,0 +1,8 @@
+function Pillow(name, description, type, shape, color, image) {
+  this.name = name;
+  this.description = description;
+  this.type = type;
+  this.shape = shape;
+  this.color = color;
+  this.image = image;
+}
