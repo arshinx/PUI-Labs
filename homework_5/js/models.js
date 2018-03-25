318fa7d8e526fc8ca5f970c5a5826ca58d81870c
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-25 17:47:24 -0400

    Pillow Object

--- homework_5/js/models.js
+++ homework_5/js/models.js
@@ -1,10 +1,16 @@
 // --- Pillows --- //
 
 // Pillow (Constructor)
-function Pillow(name, description, type, shape, color, image) {
-  this.name = name;
-  this.description = description;
-  this.type = type;
+function Pillow(name, description, prices, shapes, colors, images) {
+  this.name = name; // Distinct, remains same per product
+  this.description = description; // Changes based on product
+  this.price = prices; // 4 Choices, changes based on shape
+  this.shape = shapes; // 4 Available Shapes, corresponding to image files
+  this.color = colors; // 4 choices, adds tint to each piece
+  this.image = images; // 4 images each corresponding to the indexed-shape
+  this.currentState = currentState; // [price, shape, color, image]
+}
+
   this.shape = shape;
   this.color = color;
   this.image = image;
