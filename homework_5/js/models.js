66322b4e0078d78cafc837e26cc9fc6774912874
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-25 18:25:42 -0400

    current state

--- homework_5/js/models.js
+++ homework_5/js/models.js
@@ -8,7 +8,7 @@ function Pillow(name, description, prices, shapes, colors, images) {
   this.shape = shapes; // 4 Available Shapes, corresponding to image files
   this.color = colors; // 4 choices, adds tint to each piece
   this.image = images; // 4 images each corresponding to the indexed-shape
-  this.currentState = currentState; // [price, shape, color, image]
+  this.currentState = currentState; // ["price", "shape", "color", "image"]
 }
 
 // Change Shape (and Image)
