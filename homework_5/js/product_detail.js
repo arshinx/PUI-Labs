b8a54b9baba5d49fb1dd2c909396703837342506
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-26 00:57:16 -0400

    Product Detail Page

---
+++ homework_5/js/product_detail.js
@@ -0,0 +1,7 @@
+// Product Detail Page
+// -- Use JQuery
+$(document).ready(function() {
+
+  var selectedProduct = JSON.parse(localStorage.getItem("selectedProduct"));
+  console.log(selectedProduct);
+});
