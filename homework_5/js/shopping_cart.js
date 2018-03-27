67437963afee7292eae908293b23c94a3607fc62
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-27 12:33:41 -0400

    Jquery setup

--- homework_5/js/shopping_cart.js
+++ homework_5/js/shopping_cart.js
@@ -21,3 +21,6 @@ function showCartItem(item, index) {
   var cart = $(".cart");
   cart.append("<div class=cart-item id=" + index + "><div class=main-content id=wrapper><div class=right-section><h2 class=product-name>" + item.name + "</h2><h3 class=product-price>$" + item.currentState[0] + ".00</h3><h4><span>" + item.currentState[2] + "</span> | <span>" + item.currentState[1] + "</span></h4><button class=delete-item>Remove - X</button></div><div class=left-section><img alt=pillow class=product-image height=200 src='../" + item.currentState[3] + "' width=200></div><div id=cleared></div></div><span class=horizontal-line></span></div>");
 }
+// HTML Loaded
+$(document).ready(function() {
+});
