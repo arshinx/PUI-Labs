794f044ba39adf96014ee8e6fd3a9f261b424fed
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-27 13:30:49 -0400

    Imitate Wishlist functionality

--- homework_5/js/wishlist.js
+++ homework_5/js/wishlist.js
@@ -1,35 +1,35 @@
-// Shopping Cart
+// Shopping wishlist
 
-var cart = JSON.parse(localStorage.getItem("cart"));
-console.log(cart);
+var wishlist = JSON.parse(localStorage.getItem("wishlist"));
+console.log(wishlist);
 
-// Calculates and Displays total price of cart items
+// Calculates and Displays total price of wishlist items
 function totalPrice() {
   // Properties
   var totalPrice = 0;
-  cart = JSON.parse(localStorage.getItem("cart"));
+  wishlist = JSON.parse(localStorage.getItem("wishlist"));
   // Calculate Total Price
-  for (var i = 0; i < cart.length; i++) {
-    totalPrice += cart[i].currentState[0];
+  for (var i = 0; i < wishlist.length; i++) {
+    totalPrice += wishlist[i].currentState[0];
   }
   // Display Total Price
   $(".total-price").html("Total: $" + totalPrice + ".00");
 }
 
-// Displays a cart item with all its properties, tracks by id:index mapping
-function showCartItem(item, index) {
-  var cart = $(".cart");
-  cart.append("<div class=cart-item id=" + index + "><div class=main-content id=wrapper><div class=right-section><h2 class=product-name>" + item.name + "</h2><h3 class=product-price>$" + item.currentState[0] + ".00</h3><h4><span>" + item.currentState[2] + "</span> | <span>" + item.currentState[1] + "</span></h4><button class=delete-item>Remove - X</button></div><div class=left-section><img alt=pillow class=product-image height=200 src='../" + item.currentState[3] + "' width=200></div><div id=cleared></div></div><span class=horizontal-line></span></div>");
+// Displays a wishlist item with all its properties, tracks by id:index mapping
+function showwishlistItem(item, index) {
+  var wishlist = $(".wishlist");
+  wishlist.append("<div class=wishlist-item id=" + index + "><div class=main-content id=wrapper><div class=right-section><h2 class=product-name>" + item.name + "</h2><h3 class=product-price>$" + item.currentState[0] + ".00</h3><h4><span>" + item.currentState[2] + "</span> | <span>" + item.currentState[1] + "</span></h4><button class=delete-item>Remove - X</button></div><div class=left-section><img alt=pillow class=product-image height=200 src='../" + item.currentState[3] + "' width=200></div><div id=cleared></div></div><span class=horizontal-line></span></div>");
 }
 
 // On click - delete
 $(document).on("click", ".delete-item", function() {
   // Delete element with id #
   var index = $(this).parent().parent().parent().attr('id');
-  //cart = cart.splice(index, 1);
-  console.log(cart.splice(index, 1));
-  // Save Cart
-  localStorage.setItem("cart", JSON.stringify(cart));
+  //wishlist = wishlist.splice(index, 1);
+  console.log(wishlist.splice(index, 1));
+  // Save wishlist
+  localStorage.setItem("wishlist", JSON.stringify(wishlist));
   // Remove Element from DOM
   $(this).parent().parent().parent().remove();
   // Reload Page
@@ -38,11 +38,11 @@ $(document).on("click", ".delete-item", function() {
 
 // HTML Loaded
 $(document).ready(function() {
-  // Cart
-  cart = JSON.parse(localStorage.getItem("cart"));
+  // wishlist
+  wishlist = JSON.parse(localStorage.getItem("wishlist"));
   // Show all Items
-  for (var i = 0; i < cart.length; i++) {
-    showCartItem(cart[i], i);
+  for (var i = 0; i < wishlist.length; i++) {
+    showwishlistItem(wishlist[i], i);
   }
   // Show Total Price
   totalPrice();
