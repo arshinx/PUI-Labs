7ee9af28b37d480fb888beb4042932a5530d2daf
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-27 11:13:01 -0400

    Rename Folder - Google Maps API

--- week10_lab_googleMapsAPI/main.js
+++ week10_lab_googleMapsAPI/main.js
@@ -4,11 +4,6 @@
  * Small javascript demo of the Google Map API
  */
 
-
-
-//WRITE NEW CODE HERE
-//uncomment the code below for var myHome
-//update the latitude and longitude for where you want the center of your map to be
 var myHome = {lat: 36.616408, lng: -121.901832};
 
 function myMap() {
@@ -18,11 +13,6 @@ function myMap() {
 		zoom:7, //specifies map initial zoom level
 	};
 
-	//WRITE NEW CODE HERE
-	//uncomment code below
-	//first select the div we created for the google map on index.html
-	//then create a new google map passing in two arguments:
-	//1) where you want the map to render (the div) and 2) the map properties variable
 	var mapElement = document.getElementById('googleMap');
 	var map = new google.maps.Map(mapElement, mapProp);
 }
