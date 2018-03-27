f38ec266c87d09d5dc4c85332e45025c5cb2ae65
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-27 12:26:16 -0400

    persist items for re-visits

--- homework_5/js/browse.js
+++ homework_5/js/browse.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function() {
 
+  var pillowSet;
+  // If visitor has visited this page before, persist items
+  // Note: In case visitor selects product and comes back, they can continue
+  if (JSON.parse(localStorage.getItem("pillowSet"))) {
+    pillowSet = JSON.parse(localStorage.getItem("pillowSet"));
+  } else {
+    // Generate Random pillows
+    pillowSet = [
+      generateRandomPillow(),
+      generateRandomPillow(),
+      generateRandomPillow()
+    ];
 
 
 
