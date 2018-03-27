7ee3421b6b8933ba670cfef707010b82c83d69fe
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-27 12:33:24 -0400

    show cart item func

--- homework_5/js/shopping_cart.js
+++ homework_5/js/shopping_cart.js
@@ -15,3 +15,9 @@ function totalPrice() {
   // Display Total Price
   $(".total-price").html("Total: $" + totalPrice + ".00");
 }
+
+// Displays a cart item with all its properties, tracks by id:index mapping
+function showCartItem(item, index) {
+  var cart = $(".cart");
+  cart.append("<div class=cart-item id=" + index + "><div class=main-content id=wrapper><div class=right-section><h2 class=product-name>" + item.name + "</h2><h3 class=product-price>$" + item.currentState[0] + ".00</h3><h4><span>" + item.currentState[2] + "</span> | <span>" + item.currentState[1] + "</span></h4><button class=delete-item>Remove - X</button></div><div class=left-section><img alt=pillow class=product-image height=200 src='../" + item.currentState[3] + "' width=200></div><div id=cleared></div></div><span class=horizontal-line></span></div>");
+}
