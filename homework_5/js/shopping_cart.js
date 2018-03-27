4c772b8108d1fe3f7a744a0ca29dfba1be46f8a6
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-27 12:33:05 -0400

    calc total cart price

--- homework_5/js/shopping_cart.js
+++ homework_5/js/shopping_cart.js
@@ -3,3 +3,13 @@
 var cart = JSON.parse(localStorage.getItem("cart"));
 console.log(cart);
 
+// Calculates and Displays total price of cart items
+function totalPrice() {
+  // Properties
+  var totalPrice = 0;
+  cart = JSON.parse(localStorage.getItem("cart"));
+  // Calculate Total Price
+  for (var i = 0; i < cart.length; i++) {
+    totalPrice += cart[i].currentState[0];
+  }
+}
