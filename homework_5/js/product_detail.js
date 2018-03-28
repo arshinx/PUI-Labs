42f98c81125ccaec569763fbde724d58ab60be63
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-28 00:21:49 -0400

    Logging

--- homework_5/js/product_detail.js
+++ homework_5/js/product_detail.js
@@ -38,6 +38,7 @@ function changeColor(color) {
 $(document).ready(function() {
 
   // Get the selected product / pillow
+  console.log(JSON.parse(localStorage.getItem("selectedProduct")));
   var selectedProduct = JSON.parse(localStorage.getItem("selectedProduct"));
 
   // name
